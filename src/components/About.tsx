fed3dd4ec9b7eaada4ea633808f8a5c70f93e95f
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2020-09-22 00:12:52 -0700

    create component files and page layout

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const About: FunctionComponent = () => {
+  return(
+    <div>
+      <h1>About Section</h1>
+    </div>
+  );
+}
+
+export default About;
