7a1197d039ab4033977173f0158e0ce968733e31
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2021-01-27 21:01:44 -0800

    Add new resume

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import '../styles/NavBar.css';
-const pdfFile = require('../assets/pdfs/Cruz_Rodriguez_Software_Engineer.pdf');
+const pdfFile = require('../assets/pdfs/Cruz_Rodriguez_Software_Eng.pdf').default;
 
 interface NavBarState {
   responsiveClass: string;
